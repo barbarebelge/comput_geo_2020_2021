57e8aae20042e16916cb6a501649b6149fb8a80e
romain <romain.perret@ulb.be>
2020-11-21 12:48:25 +0100

    added DCEL code and fixed bugs with edge intersection detection when they have a common point

--- geometry.js
+++ geometry.js
@@ -125,7 +125,9 @@ function getOrientation(p1, pAngle, p2) {
 
 
 
-/** Sort the points rtadially according to the leftmost point passed as argument. */
+/** 
+ * Sort the points rtadially according to the leftmost point passed as argument.
+ */
 function sortPointsRadially(pointArray, leftMostPointIdx) {
 
     // make a shallow copy of the array
@@ -150,7 +152,8 @@ function sortPointsRadially(pointArray, leftMostPointIdx) {
 }
 
 
-/** Find the ordered set of points defining the convex hull,
+/** 
+ * Find the ordered set of points defining the convex hull,
  * the leftmost point of the set being the first in the list.
  */
 function getGrahamScanConvexHull(points, leftMostPointIdx) {
@@ -181,6 +184,44 @@ function getGrahamScanConvexHull(points, leftMostPointIdx) {
     
     return convexHullPoints;
 }
+
+
+/**
+ * Performs a Graham scan on the point set in order to create both the convex hull
+ * and the triangulation of the point set.
+ */
+function getGrahamScanTriangulation(points, leftMostPointIdx)
+{
+    let dcel = new DCELGraph();
+
+    let convexHullPoints = [];
+
+    if (points.length <= 2)
+    {
+        console.log("Not enough points to make convex hull or triangulation");
+    }
+
+    else
+    {
+        var sortedPoints = sortPointsRadially(points, leftMostPointIdx);
+        console.log("sorted points:", sortedPoints);
+        convexHullPoints = [sortedPoints[0], sortedPoints[1]];
+
+        for (let i = 2; i < sortedPoints.length; ++i) 
+        {
+            while (isRightTurn(convexHullPoints[convexHullPoints.length - 2],
+            convexHullPoints[convexHullPoints.length - 1], sortedPoints[i])) 
+            {
+                convexHullPoints.pop();
+            }
+            convexHullPoints.push(sortedPoints[i]);
+        }
+        
+    }
+    
+    return convexHullPoints;
+}
+
 
 
 /**
@@ -301,4 +342,148 @@ function getAllCombinationsOf(list){
         }
     }
     return combinations;
+}
+
+
+
+/** 
+ * Returns true if the first argument is in between the two other arguments. 
+ * This method will behave as expected no matter what the order (min or max) of the bound arguments val1 and val2.
+ */
+function inInterval(val, val1, val2) 
+{
+  return (val <= val1 && val >= val2) || (val >= val1 && val <= val2);
+}
+
+
+/**
+ * Returns the magintude of a line segment, which is its length in the euclidean plane.
+ */
+function lineMagnitude(x1, y1, x2, y2) 
+{
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+/**
+ * Returns the distance from a point to a line.
+ */
+function pointLineDistance(px, py, x1, y1, x2, y2) 
+{
+    let distance = null;
+    let magnitude = lineMagnitude(x1, y1, x2, y2);
+
+    if (magnitude < 0.00000001) 
+    {
+        distance = 9999;
+        return distance;
+    }
+
+    let u1 = (px - x1) * (x2 - x1) + (py - y1) * (y2 - y1);
+    let u = u1 / (magnitude * magnitude);
+
+    if (u < 0.00001 || u > 1) 
+    {
+        let ix = lineMagnitude(px, py, x1, y1);
+        let iy = lineMagnitude(px, py, x2, y2);
+
+        if (ix > iy) 
+        {
+            distance = iy;
+        } 
+        else 
+        {
+            distance = ix;
+        }
+    } 
+
+    else 
+    {
+        let ix = x1 + u * (x2 - x1);
+        let iy = y1 + u * (y2 - y1);
+        distance = lineMagnitude(px, py, ix, iy);
+    }
+
+    return distance;
+}
+
+
+/** 
+ * Returns true if the coordinates x and y are situated on the given segment, allowing the x and y deltas to fluctuate using
+ * a given margin. 
+ */
+function coordOnSegment(x, y, segment, clickMargin = POINT_RADIUS) {
+    let res = null;
+    let p1 = segment.p1;
+    let p2 = segment.p2;
+    let distance = pointLineDistance(x, y, p1.x, p1.y, p2.x, p2.y);
+
+    if ( dist(x, y, p1.x, p1.y) < POINT_RADIUS || dist(x, y, p2.x, p2.y) < POINT_RADIUS ) 
+    {
+        // console.log("same as one extreme point");
+        res = null;
+    }
+
+    // x value in segment's click margin and
+    // y value is between the segment extremities
+    else if ( inInterval(x, p1.x + clickMargin, p1.x - clickMargin) && inInterval(y, p1.y, p2.y) )
+    {
+        // console.log("x in segment's click margin");
+        res = new Point(p1.x, y);
+    }
+
+    // same y value and x value is between the segment extremities
+    else if ( inInterval(y, p1.y + clickMargin, p1.y - clickMargin) && inInterval(x, p1.x, p2.x) )
+    {
+        // console.log("y in segment's click margin");
+        res = new Point(x, p1.y);
+    } 
+
+    // inInterval(x, segment.p1.x + clickMargin, segment.p2.x - clickMargin) &&
+    // inInterval(y, segment.p1.y + clickMargin, segment.p2.y - clickMargin) &&
+    else if (distance < clickMargin) 
+    {
+        // let segmentSlope = (p2.x - p1.x)/(p2.y - p1.y);
+        // let newPtY = segmentSlope.
+        // console.log("point(x,y) is not in the segment click margin");
+        res = new Point(x, y);
+    }
+
+    return res;
+}
+
+
+/** 
+ * Returns true if the two segments intersect, false otherwise.
+ * The two segments will intersect if the orientations are different for the first segment according to each extremity of the second segment,
+ * and similarly the orientations are different for the second segment according to each extremity of the first segment
+ */
+function segmentsIntersect(seg1, seg2, allowAligned = true) 
+{
+    console.log("******");
+
+    let orientDifferent = orientationsDifferent(seg1, seg2, allowAligned);
+    let reverseOrientDifferent = orientationsDifferent(seg2, seg1, allowAligned);
+
+    return orientDifferent && reverseOrientDifferent;
+}
+
+/** 
+ * Returns true if the orientations of are different for the first segment according to each extremity of the second segment.
+ * If the allow aligned parameter is false, the orientation of both segments will be considered the same and the return value
+ * will thus be false.
+ */
+function orientationsDifferent(seg1, seg2, allowAligned = true)
+{
+    seg1Orient1 = getOrientation(seg1.p1, seg1.p2, seg2.p1);
+    seg1Orient2 = getOrientation(seg1.p1, seg1.p2, seg2.p2);
+    console.log(seg1Orient1);
+    console.log(seg1Orient2);
+    seg1AlignmentDetected = (seg1Orient1 === ORIENT.ALIGNED || seg1Orient2 === ORIENT.ALIGNED);
+    
+    if (seg1AlignmentDetected && ! allowAligned)
+    { 
+        return false;
+    }
+
+    return getOrientation(seg1.p1, seg1.p2, seg2.p1) !== getOrientation(seg1.p1, seg1.p2, seg2.p2);
 }
